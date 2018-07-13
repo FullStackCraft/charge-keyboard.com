b6ba73a31b0533c4e6a14d8cf29c01d2f178c026
Chris <frewin.christopher@gmail.com>
2018-07-13 12:47:33 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './styles/App.css';
 
-// external library requires() more test
+// external library requires() more test test
 const axios = require('axios');
 const validator = require("email-validator");
 
